6756915475144cc7ad9e22dec7b373e1a60e7853
KyAnh <kyanh180320@gmail.com>
2023-05-16 23:16:33 +0700

    Init project

---
+++ Assets/Scripts/Parralax.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parralax : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 startPos;
+    float spriteHeight;
+    [SerializeField] private float parralaxSpeed;
+    void Start()
+    {
+        startPos= transform.position;
+        spriteHeight = GetComponent<SpriteRenderer>().size.y;
+        print(spriteHeight);
+        print(startPos.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime * parralaxSpeed);
+        if (transform.position.y < startPos.y - spriteHeight)
+        {
+            transform.position = startPos;
+        }
+    }
+}
