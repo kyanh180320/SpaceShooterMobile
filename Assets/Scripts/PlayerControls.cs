a207bd5c65a5c8448fa3c6ae4178a9579f7aac0c
kyanh <kyanh180320@gmail.com>
2023-05-25 17:46:04 +0700

    Move Player with Touch

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 using UnityEngine.InputSystem.EnhancedTouch;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
 
 public class PlayerControls : MonoBehaviour
 {
@@ -18,12 +20,24 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Touch.fingers[0].isActive)
+        if (Touch.fingers[0].isActive) // check has fingers[0]
         {
-            Touch myTouch = Touch.activeTouches[0];
+            Touch myTouch = Touch.activeTouches[0]; //get all infor of fingers[0]
             Vector3 touchPos = myTouch.screenPosition;
             touchPos = cameraMain.ScreenToWorldPoint(touchPos);
-            transform.position = new Vector3(touchPos.x, touchPos.y, 0);
+            //transform.position = new Vector3(touchPos.x, touchPos.y, 0);
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                offset = transform.position - touchPos;
+            }
+            else if (myTouch.phase == TouchPhase.Moved)
+            {
+                transform.position  = new Vector3(touchPos.x + offset.x, touchPos.y+offset.y, 0);
+            }
+            else if(myTouch.phase == TouchPhase.Ended)
+            {
+                offset = transform.position;
+            }
         }
         
 
