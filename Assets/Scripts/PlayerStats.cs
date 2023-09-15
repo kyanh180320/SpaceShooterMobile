108e10afb13728c3433293a3fe8476177a9552d9
KyAnh <kyanh180320@gmail.com>
2023-09-15 17:36:04 +0700

    HealthBar UI

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private Image healthFill;
     [SerializeField] float maxHealth;
     float health;
     void Start()
     {
         health = maxHealth;
+        healthFill.fillAmount = health / maxHealth;
     }
 
     // Update is called once per frame
     public void TakeDamage(float damage)
     {
         health -= damage;
+        healthFill.fillAmount = health / maxHealth;
         if(health <=0)
         {
             Destroy(gameObject);
